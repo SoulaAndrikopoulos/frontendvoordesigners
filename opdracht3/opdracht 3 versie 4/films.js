3577fe61aebffa4f083cdcd797cb281e6e3e42f3
SoulaAndrikopoulos <43676124+SoulaAndrikopoulos@users.noreply.github.com>
2019-06-18 12:33:20 +0200

    Add files via upload

--- opdracht3/opdracht 3 versie 4/films.js
+++ opdracht3/opdracht 3 versie 4/films.js
@@ -1,3 +1,4 @@
+// Code verkregen uit de tutorial:
 var section = document.querySelector('section');
 var requestURL = 'https://koopreynders.github.io/frontendvoordesigners/opdracht3/json/movies.json';
 
@@ -8,36 +9,40 @@ request.responseType = 'json';
 request.send();
 console.log('request', request);
 
+//De functie van de load/GIF wordt hier aangemaakt
 request.onload = function () {
     populateSection(request.response);
     console.log(request.response);
 
 }
-
+ //De event load wordt aan het html element met class "loader" toegepast
 window.addEventListener("load", function () {
     const loader = document.querySelector(".loader");
-    loader.className += " hidden"; // class "loader hidden"
+    loader.className += " hidden"; 
 });
 
 //de functie van de films wordt hier aangemaakt
 function populateSection(json0bj) {
     console.log("function populateSection", json0bj);
 
-    //stap 1: maak elementen aan, deze worden gekoppeld aan de html
+    //stap 1: maak elementen aan, deze komen uit de Json data en worden gekoppeld aan de html
+
+    //film 1
     var titel = document.createElement('h2');
     var img = document.createElement('img');
     var plot = document.createElement('p');
 
-
+    //film 2
     var titel1 = document.createElement('h2');
     var img1 = document.createElement('img');
     var plot1 = document.createElement('p');
 
+    //film 3
     var titel2 = document.createElement('h2');
     var img2 = document.createElement('img');
     var plot2 = document.createElement('p');
 
-    //stap 2: Json koppelen aan html, films worden uit de Array gehaald
+    //stap 2: Json koppelen aan html,  de films worden hier uit de Array gehaald
     titel.textContent = json0bj[0].title;
     img.setAttribute("src", json0bj[0].cover);
 
@@ -50,7 +55,8 @@ function populateSection(json0bj) {
     img2.setAttribute("src", json0bj[2].cover);
     plot2.textContent = 'Plot: ' + json0bj[2].simple_plot;
 
-    //stap 3: voeg de gemaakte elementen aan section toe uit de html
+    //stap 3: voeg de gemaakte elementen aan de section toe. Dit is een HTML element. De Json data wordt hier ingeladen.
+    
     section.appendChild(titel);
     section.appendChild(img);
     section.appendChild(plot);
@@ -67,11 +73,10 @@ function populateSection(json0bj) {
 
 
 //Met deze code kan ik de buttons voor mijn "night and daymode" aansturen om de achtergrond van kleur te veranderen.
-document.getElementById("daymode").addEventListener("click", function (){
- document.body.style.background = "#98a0a0";   
+document.getElementById("daymode").addEventListener("click", function () {
+    document.body.style.background = "#98a0a0";
 })
 
-document.getElementById("nightmode").addEventListener("click", function (){
-document.body.style.background = "#000000";  
+document.getElementById("nightmode").addEventListener("click", function () {
+    document.body.style.background = "#000000";
 })
-
